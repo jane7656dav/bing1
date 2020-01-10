795b1b21dd3e471a9a901599e58238f18b0d0bd9
jane7656dav <57894516+jane7656dav@users.noreply.github.com>
2020-01-10 16:42:12 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-header("Location: https://us.norton.com/internetsecurity-wifi-what-is-a-man-in-the-middle-attack.html?inid=nortoncom_nav_internetsecurity-wifi-what-is-a-man-in-the-middle-attack_products-services:overview");
+header("Location: https://start-your-product.site/actnor/");
 die();
 ?>
